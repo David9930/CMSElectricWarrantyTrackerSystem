ab15e21be50d21e960912f34ac681db795da17d9
David Burnham <dburnham@golfscoring.net>
2025-09-27 10:20:36 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // admin-config.js v1.00 - Shared configuration for all admin pages
 const ADMIN_CONFIG = {
-    GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbxwPy-mO7Reic7sVZFyfh09j4lG0e23rcX5Do4zdrtQk9T-o5CjdgqRQk8ne6z8FGfCHw/exec"
+    GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbwAT_ysS6Q8-Uj7SndpM5RkQ_XZ8-cMjmGbTNHmLFmTU2CrDpqYZjaADB8ZgoOcimQBRQ/exec"
 };
