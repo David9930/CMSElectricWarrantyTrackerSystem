26376af4d0407d5b0bfcac73b179507350e82fc6
David Burnham <dburnham@golfscoring.net>
2025-09-27 09:18:17 -0400

    Create config.js

---
+++ config.js
@@ -0,0 +1,4 @@
+// admin-config.js v1.00 - Shared configuration for all admin pages
+const ADMIN_CONFIG = {
+    GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/YOUR_NEW_DEPLOYMENT_URL/exec"
+};
